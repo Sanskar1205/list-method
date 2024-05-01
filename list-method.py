5e69968d800aee97da91312629d15c5e1309c4c9
Sanskar <sanskarmp1215@gmail.com>
2024-05-01 15:56:35 +0530

    Add my Python file

---
+++ list-method.py
@@ -0,0 +1,40 @@
+lst=[1,4,5,3,7,9,4,8]
+# lst.sort()
+# print(lst)
+
+# for decrasing order
+# lst.sort(reverse=True)
+# print(lst)
+
+# for reverse
+# lst.reverse()
+# print(lst)
+
+# index return position number from zero
+# print(lst.index(7))
+
+# count returns how many times number will come
+# print(lst.count(4))
+
+# copy
+# lst2=lst.copy()
+# print(lst)
+# print(lst2)
+
+# append can add string in last 
+# lst.append(10)
+# print(lst)
+
+# insert help to return jis number ya position pe chaho wha pe item mil jayga
+# lst.insert(3, "sanskar")
+# print(lst)
+
+# extend
+# lst2=[4,2,5,2,6,32,6,3]
+# lst.extend(lst2)
+# print(lst)
+
+
+# concatenating two line
+# lst2=["sanskar", "kabir", "harry"]
+# print(lst + lst2)
